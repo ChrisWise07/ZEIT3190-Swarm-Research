28985e30b77743201fa19544060f6dab65a5f987
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2022-02-26 08:48:38 +1100

    more foundations

--- swarm_project/__init__.py
+++
@@ -1,1 +0,0 @@
-from tiled_environment import *
--- swarm_project_tests/tiled_environment_tester.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import swarm_project
-
-class tiled_environment_tester(unittest.TestCase):
-    def test_tiled_environment_object_is_created(self):
-        tiled_environment = tiled_environment()
-
-if __name__ == '__main__':
-    unittest.main()
