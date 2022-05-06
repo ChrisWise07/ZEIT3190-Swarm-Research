492cc7004550092743e8c75b633ec1a6e78e8005
ChrisWise07 <90012389+ChrisWise07@users.noreply.github.com>
2022-05-06 16:21:45 +1000

    Added updating to environments for skill 2

--- skill_training_modules/skill_training_environments/sense_broadcast_trainer_env.py
+++ skill_training_modules/skill_training_environments/sense_broadcast_trainer_env.py
@@ -6,6 +6,7 @@ from gym import spaces
 from stable_baselines3 import PPO
 import wandb
 import random
+import scipy.stats as stats
 
 ROOT_DIRECTORY = os.path.dirname(os.getcwd())
 sys.path.append(ROOT_DIRECTORY)
@@ -17,6 +18,16 @@ from environment_agent_modules import (
     create_clustered_inital_observation_not_useful_tile_grid,
     return_ratio_of_white_to_black_tiles,
 )
+
+environment_type_list = [
+    create_nonclustered_tile_grid,
+    create_clustered_inital_observation_useful_tile_grid,
+    create_clustered_inital_observation_not_useful_tile_grid,
+]
+
+
+def inverse_sigmoid_for_weighting(broadcast_percentage: float) -> float:
+    return 100 / ((1 + np.exp(0.1 * (broadcast_percentage - 50))) + 0.00001)
 
 
 class SenseBroadcastTrainer(gym.Env):
@@ -36,6 +47,13 @@ class SenseBroadcastTrainer(gym.Env):
         self.max_num_steps = max_num_of_steps
         self.width, self.height = width, height
         self.num_of_swarm_agents = num_of_swarm_agents
+        self.environment_type_weighting = [33, 33, 33]
+        (
+            self.previous_true_positives,
+            self.previous_false_positives,
+            self.previous_true_negatives,
+            self.previous_false_negatives,
+        ) = (0, 0, 0, 0)
 
     def set_model(self, model: PPO):
         self.model = model
@@ -67,25 +85,25 @@ class SenseBroadcastTrainer(gym.Env):
                 reward = -1
         else:
             if agent_opinion == self.correct_opinion:
-                self.broadcast_true_positves += 1
+                self.broadcast_true_positives += 1
                 reward = 1
             else:
-                self.broadcast_false_positves += 1
+                self.broadcast_false_positives += 1
                 reward = -1
 
         wandb.log(
             {
                 "broadcast_accuracy": (
-                    self.broadcast_true_positves + self.broadcast_true_negatives
+                    self.broadcast_true_positives + self.broadcast_true_negatives
                 )
                 / (
-                    self.broadcast_true_positves
+                    self.broadcast_true_positives
                     + self.broadcast_false_negatives
                     + self.broadcast_true_negatives
-                    + self.broadcast_false_positves
+                    + self.broadcast_false_positives
                 ),
-                "broadcast_true_positives": self.broadcast_true_positves,
-                "broadcast_false_positives": self.broadcast_false_positves,
+                "broadcast_true_positives": self.broadcast_true_positives,
+                "broadcast_false_positives": self.broadcast_false_positives,
                 "broadcast_true_negatives": self.broadcast_true_negatives,
                 "broadcast_false_negatives": self.broadcast_false_negatives,
             }
@@ -95,6 +113,17 @@ class SenseBroadcastTrainer(gym.Env):
 
         if self.num_steps == self.max_num_steps:
             self.done = True
+            broadcast_accuracy = (
+                self.broadcast_true_positives + self.broadcast_true_negatives
+            ) / (
+                self.broadcast_true_positives
+                + self.broadcast_false_negatives
+                + self.broadcast_true_negatives
+                + self.broadcast_false_positives
+            )
+            self.environment_type_weighting[self.index_of_environment] = round(
+                inverse_sigmoid_for_weighting(broadcast_accuracy * 100)
+            )
 
         self.position_of_swarm_agent_to_train = random.randint(
             0, self.num_of_swarm_agents - 1
@@ -115,13 +144,17 @@ class SenseBroadcastTrainer(gym.Env):
         self.max_num_steps = int(self.max_num_steps)
         self.decision_correctness_tracker = np.zeros(self.max_num_steps)
 
-        self.tile_grid = random.choice(
-            [
-                create_nonclustered_tile_grid,
-                create_clustered_inital_observation_useful_tile_grid,
-                create_clustered_inital_observation_not_useful_tile_grid,
-            ]
-        )(self.width, self.height, random.uniform(0.0, 1.0))
+        self.index_of_environment = random.choices(
+            [0, 1, 2],
+            weights=self.environment_type_weighting,
+            k=1,
+        ).pop(0)
+
+        self.tile_grid = environment_type_list[self.index_of_environment](
+            self.width,
+            self.height,
+            stats.truncnorm.rvs((0 - 0.5) / 1, (1 - 0.5) / 1, 0.5, 1),
+        )
 
         self.correct_opinion = round(
             return_ratio_of_white_to_black_tiles(self.tile_grid)
@@ -142,8 +175,8 @@ class SenseBroadcastTrainer(gym.Env):
             for _ in range(self.num_of_swarm_agents)
         ]
 
-        self.broadcast_true_positves = 0
-        self.broadcast_false_positves = 0
+        self.broadcast_true_positives = 0
+        self.broadcast_false_positives = 0
         self.broadcast_true_negatives = 0
         self.broadcast_false_negatives = 0
 
